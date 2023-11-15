fbafbff4878055bd6956ed2f7c7d8e6716353faf
Loedn <loedn@pm.me>
2023-11-15 10:11:25 +0100

    done stuff

--- hello_world.exs
+++ hello_world.exs
@@ -4,7 +4,7 @@
 1.0 # float, it also supports e for scientific notation i.e. 1.0e
 true # boolean
 :atom # atom / symbol
-"holy" # string
+"holy" # string, they are utf-8 encoded
 [1,2,3] # list
 {1,2,3} # tuple
 
@@ -32,3 +32,25 @@ y = "world"
 
 # x + y doesn't work
 x <> y # concatenation of strings -> "helloworld"
+"#{x} #{y}" # interpolation -> ruby AF
+"I hate number #{a}" # it supports interpolation for any datatype that can be converted into string
+"\n" # line break
+byte_size(x) # 5 -> number of bytes of "hello" this yields different results from a String.length if utf-8 representations occupy more than 1:1
+is_binary(x) # true -> strings in elixir are internally represented internally by contiguous sequences of bytes known as binaries
+
+# string module -> contains a bunch of helper functions in unicode standard
+String.length(x) # 5
+String.upcate(x) # "HELLO"
+
+# functions
+def hello() # identifier->def, arity -> () describes the number of arguments that the function takes
+
+# atoms
+:apple # atoms/symbols are a constant whose value is its own name atoms are equal if their names are equal
+:apple == :apple # true
+:apple == :pear # false
+
+# structural comparison == === != !== <= >= < > you can numbers, atoms, strings, booleans everything
+1 == 1 # true
+1 == 1.0 # true
+1 === 1.0 # false
